698355ba48217b59361d1ea4e34327eae2c7ff94
OlhaMajerski <olha1991o@gmail.com>
2024-06-13 18:14:52 +0200

    functions

--- js/module1.js
+++ js/module1.js
@@ -19,7 +19,7 @@ const balance = '1550.65eu';
 const floatedBalance = parseFloat(balance);
 console.log(floatedBalance);
 
-const userAge = prompt('Enter your age');
+/* const userAge = prompt('Enter your age');
 const result = +userAge > 18;
 const result1 = Number(userAge) > 18;
 const result2 = userAge > 18; //nejavne peretvorennia
@@ -27,7 +27,7 @@ const result3 = parseInt(userAge) > 18; //if u print 30yo, it will log true
 console.log(result);
 console.log(result1);
 console.log(result2);
-console.log(result3);
+console.log(result3); */
 
 const accountBalance = 456.51;
 const x = Math.round(accountBalance);
@@ -38,3 +38,40 @@ console.log(x, y, z, t);
 const r = 34.34567;
 const copyR = Math.round(r * 1000) / 1000;
 console.log(copyR);
+
+const min = 500;
+const max = 1000;
+const random = Math.round(Math.random() * (max - min) + min);
+console.log(random);
+const a = Math.round(Math.random() * 500);
+console.log(a);
+
+function showYourGuide() {
+  console.log('smile');
+  console.log('laugh');
+  console.log('be kind');
+  console.log('be yourself');
+}
+showYourGuide();
+
+function returnResult() {
+  const cat = 25;
+  const dog = 45;
+  const parrot = 55;
+  const zoo = cat + dog + parrot;
+  console.log(zoo);
+  return zoo;
+}
+returnResult();
+const show = returnResult();
+console.log(show);
+
+function sum(x, y) {
+  const sum = x + y;
+  return sum;
+  //console.log(x, y);
+}
+const sol = sum(10, 20);
+console.log(sol);
+
+//==========================================//
